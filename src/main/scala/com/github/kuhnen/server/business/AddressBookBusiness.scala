892fb75d015cf3a9a1f7a049efb33a4e70507c37
kuhnen <andrekuhnen@gmail.com>
2015-01-13 22:11:32 -0200

    added methods

--- src/main/scala/com/github/kuhnen/server/business/AddressBookBusiness.scala
+++ src/main/scala/com/github/kuhnen/server/business/AddressBookBusiness.scala
@@ -1,6 +1,6 @@
 package com.github.kuhnen.server.business
 
-import com.github.kuhnen.pojo.AddressBook
+import com.github.kuhnen.pojo.{User, AddressBook}
 import com.github.kuhnen.server.dao.{AddressBookDaoImpl, AddressBookDao}
 import com.twitter.util.Future
 
@@ -16,11 +16,21 @@ class AddressBookBusinessImp extends AddressBookBusiness {
 
 trait AddressBookBusiness {
 
+  implicit def fakeId(user: User) = AddressBook("fakeId", user)
+
   val dao: AddressBookDao
 
   def get(id: String): Future[Option[AddressBook]] = dao.select(id)
 
-  def list(): Future[Iterable[AddressBook]] = dao.list()
+  def list(): Future[Iterable[AddressBook]] = dao.selectAll()
+
+  //I am creating a fake id, just to be able to pass the right type to the database
+  //but the DB will be responsible about the key creation. Not the best idea
+  def insert(user: User): Future[dao.Id] = dao.insert(user)
+
+  def update(id: String, user: User) = dao.update(AddressBook(id, user))
+
+  def delete(id: String): Future[Unit] = dao.delete(id)
 
 
 
