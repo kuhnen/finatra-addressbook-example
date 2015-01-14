8516154fc529573935ec81c94cd1d2aa0a4bac5d
kuhnen <andrekuhnen@gmail.com>
2015-01-13 22:15:14 -0200

    added methods

--- src/main/scala/com/github/kuhnen/server/dao/AddressBookDao.scala
+++ src/main/scala/com/github/kuhnen/server/dao/AddressBookDao.scala
@@ -3,6 +3,8 @@ package com.github.kuhnen.server.dao
 import com.github.kuhnen.pojo.AddressBook
 import com.twitter.util.Future
 
+import scala.reflect.ClassTag
+
 /**
  * Created by kuhnen on 1/12/15.
  */
@@ -13,18 +15,31 @@ class AddressBookDaoImpl extends AddressBookDao {
 }
 
 
-trait AddressBookDao extends DB[String, AddressBook]
+trait AddressBookDao extends DB[String, AddressBook] {
+  implicit def dbId(addressBook: AddressBook) = addressBook.hashCode().toString
+}
 
 
 trait DB[K, V] {
+
+  type Id = K
+  implicit def dbId(v: V): K
+
+  implicit def valueToKeyValue(v: V): (K,V) = (v, v)
 
   protected var data: Map[K, V]
 
   def select(id: K): Future[Option[V]] = Future { data.get(id) }
 
-  def insert(id: K, value: V): Future[Unit] =  Future { data = data.updated(id, value) }
+  def insert(value: V)(implicit tag: ClassTag[K]): Future[K] = Future {
+    val (key: K, _) = value
+    data = data.updated(key, value)
+    key
+  }
 
-  def list(): Future[Iterable[V]] =  Future { data.values }
+  def update(value: V): Future[Unit] =  Future { data = data.updated(value, value) }
+
+  def selectAll(): Future[Iterable[V]] =  Future { data.values }
 
   def delete(id: K): Future[Unit] = Future { data = data - id }
 
